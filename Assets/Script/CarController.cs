309d7228964bb963befb9dbd49e1a1082c2fcb8f
ItsOxas <121440551+ItsOxas@users.noreply.github.com>
2024-01-13 21:36:37 +0200

    throttle

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float throttle;
+    public float steer;
 
-    public object car;
+    public List<WheelCollider>steeringWheels;
+    public List<WheelCollider> throttleWheels;
+    public Rigidbody rb;
+
+    public float torque = 690000f;
+    public float steerAngle = 20f;
+
+    private void Start()
+    {
+            rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        GetComponent<Rigidbody>().
+        throttle = Input.GetAxis("Vertical");
+        steer = Input.GetAxis("Horizontal");
+
+        foreach (WheelCollider wheel in throttleWheels) 
+        {
+            wheel.motorTorque = torque * Time.deltaTime * throttle;
+        }
+        foreach (WheelCollider wheel in steeringWheels)
+        {
+            wheel.GetComponent<WheelCollider>().steerAngle = 20f * steer;
+            wheel.transform.localEulerAngles = new Vector3(0, steer * steerAngle,0);
+        }
     }
 }
